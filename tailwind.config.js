bc7769d7f639d39c3605ca285038d0068815cf7c
Sahil Lamba <sahil.lamba95@gmail.com>
2022-11-08 19:51:04 +0530

    Add custom theme using Jekyll vars and Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
+  darkMode: 'class',
   content: [
     './_drafts/**/*.html',
     './_includes/**/*.html',
@@ -8,8 +11,32 @@ module.exports = {
     './*.html',
   ],
   theme: {
-    theme: {
-      extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: colors.white,
+      gray: colors.gray,
+      black: colors.black,
+      primary: {
+        light: 'var(--color-primary-light)',
+        DEFAULT: 'var(--color-primary-base)',
+        dark: 'var(--color-primary-dark)',
+      },
+      inverted: {
+        light: 'var(--color-inverted-light)',
+        DEFAULT: 'var(--color-inverted-base)',
+        dark: 'var(--color-inverted-dark)',
+      },
+      light: {
+        bright: colors.white,
+        DEFAULT: colors.gray[50],
+        dull: colors.gray[200],
+      },
+      dark: {
+        bright: colors.gray[700],
+        DEFAULT: colors.gray[800],
+        dull: colors.gray[900],
+      },
     },
   },
   plugins: [],
